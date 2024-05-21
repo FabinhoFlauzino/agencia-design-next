cd5aa1d30826824a8fb2919b2ced9db63c8e2ef6
unknown <fabioflauzino@hotmail.com>
2024-05-20 23:04:43 -0300

    feat: criando componente banner

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,6 +1,19 @@
+import styles from './Banner.module.css';
+import ImagemBanner from '../../../public/assets/banner.png';
+import Image from 'next/image';
+
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <div className={styles.banner_container}>
+      <div className={styles.banner_imagem}>
+        <Image src={ImagemBanner} alt="Imagem de um rapaz segurando notebook e caneta"/>
+      </div>
+      <div className={styles.banner_content}>
+        <p>BRANDING / UI / UX / TECNOLOGIA</p>
+        <h1>Agência de Branding</h1>
+        <span>e design digital</span>
+      </div>
+    </div>
   )
 }
 
--- src/components/Topo/index.jsx
+++ src/components/Topo/index.jsx
@@ -2,14 +2,20 @@ import Image from "next/image";
 import styles from "./Topo.module.css";
 import Logo from "../../../public/assets/logo.png";
 import Moon from "../../../public/assets/moon.png";
+import Sun from "../../../public/assets/sun.png";
+import { useState } from "react";
 
-export const Topo = () => {
+export const Topo = ({tipoTema, setState}) => {
+  const alterarState = () => {
+    tipoTema === "light-mode" ? setState("light-mode") : setState("dark-mode");
+  };
+
   return (
-    <header className={`${styles.container}`}>
+    <header className={`${styles.container} ${tipoTema ? styles.dark_mode : styles.light_mode}`}>
       <div className={styles.container_content}>
-        <Image src={Logo} className={styles.logo} />
+        <Image src={Logo} className={styles.logo} alt="Logo"/>
         <button className={styles.btn_header}>
-          <Image src={Moon} />
+          <Image src={tipoTema ? Sun : Moon} alt="Imagem botão" onClick={alterarState}/>
         </button>
       </div>
     </header>
